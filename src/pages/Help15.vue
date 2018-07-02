afcc120c8c5fe3e42a8a048ca047027ff00babe3
梁小波 <liangxb@codoon.com>
2018-07-02 19:30:12 +0800

    bugfixes

--- src/pages/Help15.vue
+++ src/pages/Help15.vue
@@ -1,7 +1,7 @@
 <template>
   <page>
     <template slot="content">
-      <help-content :title="''GPSイベントにマイページの距離値が反映されない理由は？'">
+      <help-content :title="'GPSイベントにマイページの距離値が反映されない理由は？'">
         <template slot="content">
           <div class="text">
             マイページに表示されている距離は歩数と歩幅によって計算した値です。マイページの「ウォーク/ラン」機能を使用し、「ウォーク」または「ラン」を完了した運動記録のみGPSイベントの対象データとなります。
