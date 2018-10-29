74b1bb0020e1471e07a45681b4c3d14756ff19e9
梁小波 <liangxb@codoon.com>
2018-10-29 23:21:00 +0800

    修改字体

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require("gulp")
 var s3 = require('gulp-s3')
 
 var AWS = {
-  'key':    '',
-  'secret': '',
+  'key':    'AKIAJO5PUCGDYRZV43FQ',
+  'secret': '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',
   'bucket': 'werun-h5',
   'region': 'us-east-1'
 }
