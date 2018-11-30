44d9befcfb74982ec0f2ed6c00052e0689aa8580
Slash Yang <boyang@xing-group.com>
2018-11-30 15:28:43 +0900

    modified faq

--- src/pages/Help10.vue
+++ src/pages/Help10.vue
@@ -3,7 +3,7 @@
     <template slot="content">
       <help-content :title="'アプリの歩数はiPhoneの「ヘルスケア」と差がある理由は？'">
         <template slot="content">
-          <div class="text">当日ログインアカウントを切り替えたり、異なる携帯でログインしたり、または携帯の時間帯を東京以外に設定したりしたことがあると、WeRUNの歩数と「ヘルスケア」データの間に大きな差が出る原因となります。</div>
+          <div class="text">当日ログインアカウントを切り替えたり、異なる携帯でログインしたり、または携帯の時間帯を東京以外に設定したことがあると、WeRUNの歩数と「ヘルスケア」データの間に大きな差が出る原因となります。</div>
         </template>
       </help-content>
     </template>
